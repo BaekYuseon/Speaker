73a769fd533f3eff9cda90831597a70b772579ca
BaekYuseon <cofls0518@gmail.com>
2021-05-06 17:04:18 +0900

    adsfadsf

--- js/ajax.js
+++ js/ajax.js
@@ -3,10 +3,28 @@ $('.login_button').click(function() {
     url:"../js/MOCK_DATA.json",
     dataType:"json",
     success:function(data){
-        alert('로그인 성공');
-        location.reload();
-        }, error : function() {
+        if (data['result'] == 'false'){
+            alert('로그인 실패');
+        } else if (data['result'] == 'success'){
+            alert('로그인 성공');
+            $("#login_content").html(data[id] + "님 환영합니다.");
+            location.reload("../index.html");
+        }
+    }, error : function() {
             alert('로그인 정보가 올바르지 않습니다.')
         }
     });
 });
+
+// $('.login_button').click(function() {
+//     $.ajax({
+//     url:"../js/MOCK_DATA.json",
+//     dataType:"json",
+//     success:function(data){
+//         alert('로그인 성공');
+//         location.reload();
+//         }, error : function() {
+//             alert('로그인 정보가 올바르지 않습니다.')
+//         }
+//     });
+// });
